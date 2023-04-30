2c5f4baad2c51b29553ee3ad7432fe3626defd98
Ox Noj <geneveva@gmail.com>
2023-04-29 17:46:06 -0700

    Added to schema file, editing Role and Employee tables

--- schema.sql
+++ schema.sql
@@ -8,17 +8,25 @@ CREATE TABLE department (
     name VARCHAR(30)
 )
 
+--role department_id is foreign key related to deparment primary key of id
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
+    CONSTRAINT fk_role_department,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 )
 
+--employee role_id foreign key related to table role id
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    CONSTRAINT fk_employee_role,
+    CONSTRAINT fk_employee_manager,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
