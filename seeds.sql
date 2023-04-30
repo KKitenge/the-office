ed714bf4eb4d0105051c5560dfb23b56aa92d90d
Ox Noj <geneveva@gmail.com>
2023-04-29 20:52:41 -0700

    Edited roles and added to employee sections

--- seeds.sql
+++ seeds.sql
@@ -5,15 +5,27 @@ VALUES  ('Crafting'),
         ('Horiculture'),
         ('Scribing');
 
-INSERT INTO role (title, salary, department_id)
-VALUES  ('Knit Lead', 75,000, 'Crafting'),
-        ('Crochet Lead', 80,000, 'Crafting'),
-        ('Executive Chef', 100,000, 'Gastronomy'),
-        ('Sommelier', 96,500, 'Gastronomy'),
-        ('Arborist', 85,000, 'Horiculture'),
-        ('Florist', 90,000, 'Horiculture'),
-        ('Scribe Analyst', 90,500, 'Scribing'),
-        ('Scribe Specialist', 91,500, 'Scribing'),
-        ('Liason', 110,000, 'Exploration'),
-        ('Adventurer', 115,000, 'Exploration');
+INSERT INTO role (title, salary, department_id) --department_id is to be an integer
+VALUES  ('Knit Lead', 80,000, '1'),
+        ('Crochet Lead', 75,000, '1'),
+        ('Executive Chef', 100,000, '3'),
+        ('Sommelier', 96,500, '3'),
+        ('Arborist', 85,000, '4'),
+        ('Florist', 90,000, '4'),
+        ('Scribe Analyst', 90,500, '5'),
+        ('Scribe Specialist', 91,500, '5'),
+        ('Liason', 110,000, '2'),
+        ('Adventurer', 115,000, '2');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Gia', 'Campbell', '1', NULL), -- no manager
+        ('Nikolas', 'Cassadine' '2', '1'), --Gia = manager
+        ('Victor', 'Newman', '3', NULL), 
+        ('Drucilla', 'Winters', '4', '3'),
+        ('Dorothy', 'Zbornak', '5', '6'),
+        ('Lucas', 'Black', '6', NULL),
+        ('Blanche', 'Devereaux', '7', '8'),
+        ('Rose', 'Nylund', '8', NULL),
+        ('Ichabod', 'Crane', '9', '10'),
+        ('Abbie', 'Mills', '10', NULL);
         
