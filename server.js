28cfa78279772faa8b614a8a5be1cbaee4fd0233
Ox Noj <geneveva@gmail.com>
2023-04-29 21:42:43 -0700

    Added first prompt question and choices

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Dependencies
-const express = require('express');
+const inquirer = require('inquirer');//inquirer not express
 const mysql = require('mysql12');
 const path = require('path');
 
@@ -19,40 +19,56 @@ const db = mysql.createConnection(
         // MySQL password
         password: '',
         database: 'employee_db'
-      },
-      console.log(`Connected to the employee_db database.`)
-    );
+    },
+    console.log(`Connected to the employee_db database.`)
+);
 
 //When app opens, presented with: 
-    //view all departments, 
-    //view all roles, 
-    //view all employees, 
-    //add a department, 
-    //add a role, 
-    //add an employee, 
-    //update an employee role
+//view all departments, 
+//view all roles, 
+//view all employees, 
+//add a department, 
+//add a role, 
+//add an employee, 
+//update an employee role
+const questions = [
+    {
+        type: 'list',
+        name: 'introduction'
+        message: 'Hello, what would you like to do?',
+        choices: [
+            'View all Departments',
+            'View all Roles',
+            'View all Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role'
+        ]
+    },
+];
 
 
 //When view all departments is chosen, presented with:
-      //formatted table showing department names and department ids
+//formatted table showing department names and department ids
 
 //When view all roles is chosen, presented with:
-      //the job title, role id, the department that role belongs to, and the salary for that role
+//the job title, role id, the department that role belongs to, and the salary for that role
 
 //When view all employees is chosen, presented with:
-      //a formatted table showing employee ids, first, last names, job titles, departments, salaries, and managers
+//a formatted table showing employee ids, first, last names, job titles, departments, salaries, and managers
 
 //When a department is added, prompted to:
-    //enter the name of the department and that department is added to the database
+//enter the name of the department and that department is added to the database
 
 //When a role is added, prompted to:
-      //enter the name, salary, and department for the role and that role is added to the database
+//enter the name, salary, and department for the role and that role is added to the database
 
 //When an employee is added, prompted to:
-      //enter first name, last name, role, and manager, and that employee is added to the database
+//enter first name, last name, role, and manager, and that employee is added to the database
 
 //When action is taken to update an employee role:
-      //prompted to select an employee to update and their new role and info is updated in the database 
+//prompted to select an employee to update and their new role and info is updated in the database 
 
 
 
